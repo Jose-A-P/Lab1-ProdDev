b67d50258e3ca120ee5a9fe08e2f7e7a651e34ca
Jose-A-P <jalvarezp_72@outlook.com>
2025-10-25 18:16:44 -0600

    Adicion de modelos y metricas
    Se agregaron Gradient Boosting Regressor y Random Forest Regressor.
    
    Se incluyeron las metricas de R2 y MAE en los modelos.

--- src/train.py
+++ src/train.py
@@ -2,8 +2,9 @@ import pandas as pd
 import yaml, json
 import joblib
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import root_mean_squared_error
+from sklearn.metrics import root_mean_squared_error, mean_squared_error, r2_score
 from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 
 params = yaml.safe_load(open("params.yaml"))
 df = pd.read_csv("data/processed.csv")
@@ -20,26 +21,32 @@ X_train, X_test, y_train, y_test = train_test_split(
 models_cfg = params["models"]
 results = {}
 best_model = None
-best_score = -1
+best_score = float('inf') # Inicializando con infinito, mas bajo mejor.
 best_name = ""
 
 for name, cfg in models_cfg.items():
     if name == "linear_regression":
         model = LinearRegression()
+    elif name == "random_forest_regressor":
+        model = RandomForestRegressor(**cfg)
+    elif name == "gradient_boosting_regressor":
+        model = GradientBoostingRegressor(**cfg)
     else:
         continue
 
     model.fit(X_train, y_train)
     preds = model.predict(X_test)
     score = root_mean_squared_error(y_test, preds)
+    r2 = r2_score(y_test, preds)
+    mscore = mean_squared_error(y_test, preds)
 
-    results[name] = {"RMSE": score}
+    results[name] = {"RMSE": score, "R2": r2, "MSE": mscore}
 
-    if score > best_score:
+    if score < best_score:
         best_score = score
         best_model = model
         best_name = name
 
 joblib.dump(best_model, "model.pkl")
 json.dump(results, open("metrics.json", "w"), indent=4)
-print(f"El mejor modelo es: {best_name} con un RMSE de {best_score:.4f}")
+print(f"El mejor modelo es: {best_name} con: \nRMSE de {best_score:.4f} \nR2 de {results[best_name]['R2']:.4f} \nMSE de {results[best_name]['MSE']:.4f}")
